5f7c77a1745ba621c6d75f895b59da9c004d305b
sharky <grzegorz.dyk@gmail.com>
2015-03-29 21:54:39 +0200

    #3 Some additional tests for completion added

--- src/test/java/org/intellij/vala/completion/ConstructorCompletionTest.java
+++ src/test/java/org/intellij/vala/completion/ConstructorCompletionTest.java
@@ -1,30 +1,17 @@
 package org.intellij.vala.completion;
 
-import com.google.common.collect.ImmutableList;
-import com.intellij.codeInsight.completion.CodeCompletionHandlerBase;
 import com.intellij.codeInsight.completion.CompletionType;
 import com.intellij.codeInsight.lookup.LookupElement;
-import com.intellij.codeInsight.lookup.LookupEx;
-import com.intellij.codeInsight.lookup.LookupManager;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.fileEditor.FileEditor;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.testFramework.PlatformTestCase;
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
 import org.hamcrest.CustomTypeSafeMatcher;
 import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
 
-import javax.annotation.processing.Completion;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.assertThat;
 
 
@@ -69,12 +56,24 @@ public class ConstructorCompletionTest extends LightPlatformCodeInsightFixtureTe
         doTest();
     }
 
+    public void testSingleNamedConstructorCompletionWithFullClassNameBeforeDot() {
+        doTest();
+    }
+
     public void testSingleNamedConstructorCompletionRightAfterDot() {
         myFixture.configureByFiles(this.getTestName(false) + ".vala");
 
         List<LookupElement> elements = Arrays.asList(myFixture.completeBasic());
 
         assertThat(elements, contains(lookupElement("from_string")));
+    }
+
+    public void testMultipleNamedConstructorCompletion() {
+        myFixture.configureByFiles(this.getTestName(false) + ".vala");
+
+        List<LookupElement> elements = Arrays.asList(myFixture.completeBasic());
+
+        assertThat(elements, containsInAnyOrder(lookupElement("MyClass.from_string"), lookupElement("MyClass.from_int")));
     }
 
     private static Matcher<? super LookupElement> lookupElement(String lookupString) {
---
+++ src/test/resources/org/intellij/vala/completion/test/MultipleNamedConstructorCompletion.vala
@@ -0,0 +1,12 @@
+class MyClass {
+	public MyClass.from_string(string arg) {
+	}
+
+	public MyClass.from_int(int arg) {
+
+	}
+}
+
+int main(string [] args) {
+	var obj = new MyCl<caret>
+}
